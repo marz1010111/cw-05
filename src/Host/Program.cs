218a05c770bc2249cbb708eb36413ba998f983d2
Martin Zubovskiy <118899868+marz1010111@users.noreply.github.com>
2023-09-28 19:56:55 +0300

    added cw-05

---
+++ src/Host/Program.cs
@@ -0,0 +1,48 @@
+﻿using Class;
+
+Class1 class1 = new();
+Console.WriteLine("Help: r - repaet, e - exit.");
+
+while (true)
+{
+    int a1 = 7;
+    int b1 = a1 + 1;
+
+    Console.WriteLine($"{nameof(b1)} = {b1}"); // 7
+    Console.WriteLine($"{nameof(a1)} = {a1}"); // 8
+
+    // prefix
+
+    int a2 = 7;
+    int b2 = ++a2;
+
+    Console.WriteLine($"{nameof(b2)} = {b2}"); //8
+    Console.WriteLine($"{nameof(a2)} = {a2}"); //8
+    
+    // postfix
+
+    int a3 = 7;
+    int b3 = a2++;
+
+    Console.WriteLine($"{nameof(b3)} = {b3}"); // 7
+    Console.WriteLine($"{nameof(a3)} = {a3}"); // 8
+
+
+
+
+
+
+    // -------------------------------------------
+
+    Console.Write("Your choice? > ");
+    string? inputUser = Console.ReadLine();
+
+    if (inputUser == "r")
+    {
+        continue;
+    }
+    else if (inputUser == "e")
+    {
+        break;
+    }
+}
